11d7096b6f3dfc12ab87b4aa52b7373c0e7493eb
zqx1392 <pun_sd49@hotmail.com>
2016-02-20 22:37:47 +0700

    Update myxblock.js

--- myxblock/static/js/src/myxblock.js
+++ myxblock/static/js/src/myxblock.js
@@ -1,20 +1,40 @@
 /* Javascript for MyXBlock. */
 function MyXBlock(runtime, element) {
 
-    function updateCount(result) {
-        $('.count', element).text(result.count);
-    }
+
     /* DEFINE URL(FUNCTION) TO USE FROM MYXBLOCK.PY, IN THIS CASE WE CALL FUNCTION */
     var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+    var checkUrl = runtime.handlerUrl(element,'get_answer')
+    var answers = $('input[type=radio]', element);
 
-    $('p', element).click(function(eventObject) {
+    $('.check', element).click(function(eventObject) {
         $.ajax({
             type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
+            url: checkUrl,
+            data: JSON.stringify(gatherStudentChoices()),
+            success: function (data) {
+                alert(JSON.stringify(data.Results));
+            },
+	    error: function (textStatus, errorThrown) {
+                alert ('no');
+            }
         });
     });
+    var gatherStudentChoices = function () {
+        // Grabs all selections for student's answers, and returns a mapping for them.
+        var choices = {};
+        answers.each(function(index, el) {
+            el = $(el);
+            choices[el.prop('name')] = $(checkedElement(el), element).val();
+        });
+        return choices;
+    };
+
+    var checkedElement = function (el) {
+        // Given the DOM element of a radio, get the selector for the checked element
+        // with the same name.
+        return "input[name='" + el.prop('name') + "']:checked"
+    };
 
     $('label',element).hover(function(){
 	$(this).css( "border-color", "rgb(0, 120, 167)");
