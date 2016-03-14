b7e2faf6e9b3cc0f6a22b0eeceacc66581eefb4b
zqx1392 <pun_sd49@hotmail.com>
2016-03-14 20:31:16 +0700

    add folder

---
+++ learning_path/__init__.py
@@ -0,0 +1,1 @@
+from .learning_path import QuestionBlock,LinkBlock
---
+++ setup.py
@@ -0,0 +1,40 @@
+"""Setup for learning_path XBlock."""
+
+import os
+from setuptools import setup
+
+
+def package_data(pkg, roots):
+    """Generic function to find package_data.
+
+    All of the files under each of the `roots` will be declared as package
+    data for package `pkg`.
+
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+setup(
+    name='xblock-learning_path',
+    version='0.710',
+    description='An Xblock to guide students through a course with adaptive learning path',
+    packages=[
+        'learning_path',
+    ],
+    install_requires=[
+        'XBlock',
+    ],
+    entry_points={
+        'xblock.v1': [
+            'question = learning_path:QuestionBlock',
+            'link = learning_path:LinkBlock'
+        ]
+    },
+    package_data=package_data("learning_path", ["static", "public"]),
+)
